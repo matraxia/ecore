163d965fb16fb0c2cfaa85b72a19b211de128091
mattia mesco <masa_-94@live.it>
2025-05-29 19:08:36 +0200

    first_setup: first interation with db

--- ecore/routes/user_route.py
+++ ecore/routes/user_route.py
@@ -1,11 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from ecore.service.dependencies import get_db
 
-user_router = APIRouter(prefix='user/')
+user_router = APIRouter(prefix='/user', tags=["user"])
 
 
 @user_router.get("/")
-async def root():
-    return "user", 200
+async def root(db: Session = Depends(get_db)):
+
+    query_get_user_by_id = "SELECT * FROM app_user WHERE id_user = 2"
+    result = db.execute(text(query_get_user_by_id))
+    return result.fetchone(), 200
 
 
 @user_router.post("/user")
